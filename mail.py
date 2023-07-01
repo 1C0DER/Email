a0414a3918fbf0ea3c6729fc0256ff3da19f8c6b
1C0DER <anime1028.d@gmail.com>
2023-07-01 17:49:48 +0100

    test?

--- mail.py
+++ mail.py
@@ -1,3 +1,4 @@
+"""
 import getpass
 #getting password from user
 
@@ -22,14 +23,16 @@ TO_EMAIL = "ejiro006@gmail.com"
 
 PASSWORD = getpass.getpass("Enter password: ")
 #this is to ask for password
+"""
 
-MESSAGE = """Subject: Test drive
+"""
+MESSAGE = Subject: Test drive
 Heyyo Good morning Sir 
 
 I hope you have finished filling the school documents
 
 Your village people,
-Testing testing 1,2,3"""
+Testing testing 1,2,3
 #the mail being sent (always make sure it is sent in this format)
 
 smtp = smtplib.SMTP(HOST, PORT)
@@ -71,8 +74,56 @@ smtp.quit()
 #To close the connection with the server 
 
 print('Email sent successfully :)')
+"""
 
 
 
+
+import getpass
+import smtplib
+import datetime as dt
+import time
+
+def get_password():
+    return getpass.getpass("Enter password: ")
+
+def send_email():
+    HOST = "smtp.gmail.com"
+    PORT = 587
+    FROM_EMAIL = "d9006714@gmail.com"
+    TO_EMAIL = "ejiro006@gmail.com"
+    PASSWORD = get_password()
+    MESSAGE = """Subject: Test drive
+    Heyyo Good morning Sir 
+
+    I hope you have finished filling the school documents
+
+    Your village people,
+    Testing testing 1,2,3"""
+
+    smtp = smtplib.SMTP(HOST, PORT)
+
+    status_code, response = smtp.ehlo()
+    print(f"[*] Echoing the server: {status_code} {response}")
+
+    status_code, response = smtp.starttls()
+    print(f"[*] Starting TLS connection: {status_code} {response}")
+
+    status_code, response = smtp.login(FROM_EMAIL, PASSWORD)
+    print(f"[*] Logging in {status_code} {response}")
+
+    send_time = dt.datetime(2023, 6, 26, 9, 0, 0)
+    print(send_time.timestamp())
+
+    print(time.time())
+    x = time.sleep(send_time.timestamp() - time.time())
+    print(x)
+
+    smtp.sendmail(FROM_EMAIL, TO_EMAIL, MESSAGE)
+    smtp.quit()
+
+    print('Email sent successfully :)')
+
+send_email()
 
 
