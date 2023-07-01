ad0e3b2fe3dbba6ef122ce9c3c6dfcbc1ab0cdbf
1C0DER <anime1028.d@gmail.com>
2023-07-01 17:18:07 +0100

    okay

--- mail.py
+++ mail.py
@@ -4,6 +4,10 @@ import getpass
 import smtplib
 #used to send email
 
+import datetime as dt
+
+import time
+
 HOST = "smtp.gmail.com"
 #This is the smtp server domain name(for gmail in particular)
 
@@ -13,19 +17,19 @@ PORT = 587
 FROM_EMAIL = "d9006714@gmail.com"
 #the email sending
 
-TO_EMAIL = "nuttzack12@gmail.com"
+TO_EMAIL = "ejiro006@gmail.com"
 #the email receiving
 
 PASSWORD = getpass.getpass("Enter password: ")
 #this is to ask for password
 
 MESSAGE = """Subject: Test drive
-Hey dumbass, 
+Heyyo Good morning Sir 
 
-YOYYO IT WORKS. YOU CAN STOP SUFFERING NOW
+I hope you have finished filling the school documents
 
-Your clueless past self,
-Test Account"""
+Your village people,
+Testing testing 1,2,3"""
 #the mail being sent (always make sure it is sent in this format)
 
 smtp = smtplib.SMTP(HOST, PORT)
@@ -48,8 +52,27 @@ status_code, response = smtp.login(FROM_EMAIL, PASSWORD)
 print(f"[*] Logging in {status_code} {response}")
 #print the response
 
+send_time = dt.datetime(2023, 6, 26, 9, 00, 0)
+#Okay so yeah this is in the format of Year-Month-Date-Hour-Minuite-Second
+
+print(send_time.timestamp())
+#getting the time in teh time stanp format
+
+print(time.time())
+#print current time 
+
+x = time.sleep(send_time.timestamp() - time.time())
+print(x)
+
 smtp.sendmail(FROM_EMAIL, TO_EMAIL, MESSAGE)
 #used to send the mail after a succesful login
 
 smtp.quit()
 #To close the connection with the server 
+
+print('Email sent successfully :)')
+
+
+
+
+
