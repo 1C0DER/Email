79228f47945b2dffcf3d64039116ef73017d7468
1C0DER <106778213+1C0DER@users.noreply.github.com>
2023-07-27 19:55:41 +0100

    kill me

--- mail.py
+++ mail.py
@@ -4,11 +4,17 @@ import datetime as dt
 import time
 
 def request_pass():
-    password = getpass.getpass("Enter password: ")
+    try:
+        password = getpass.getpass("Enter password: ")
+    except Exception as e:
+        print(f"Error error wee woo wee woo password error:{e}") 
     return password
 
 def message_create(from_email, to_email, subject, body):
-    message = f"Subject: {subject}\n\n{body}"
+    try:
+        message = f"Subject: {subject}\n\n{body}"
+    except Exception as e:
+        print(f"Error error wee woo wee woo message wont create:{e}") 
     return message
 
 def send_email(from_email, to_email, password, subject, body, host="smtp.gmail.com", port=587):
@@ -31,14 +37,30 @@ def send_email(from_email, to_email, password, subject, body, host="smtp.gmail.c
         print('Email sent successfully: ') 
         
     except Exception as e:
-        print(f"Error error wee woo wee woo fix this error:{e}")  
+        print(f"Error error wee woo wee woo the message wont send:{e}")  
 
 def delay_email(from_email, to_email, password, subject, body):
-    tnow = dt.datetime.now()      
-    send_time = tnow + dt.timedelta(days=365)
-    print(send_time.timestamp())
-    time.sleep((send_time - tnow).total_seconds())
-    send_email(from_email, to_email, password, subject, body)
+    try:
+        tnow = dt.datetime.now()      
+        send_time = tnow + dt.timedelta(days=365)
+        print(send_time.timestamp())
+        time.sleep((send_time - tnow).total_seconds())
+        send_email(from_email, to_email, password, subject, body)
+    except Exception as e:
+        print(f"Error error wee woo wee woo the delay wont go through:{e}") 
+
+def send_reminder(from_email, to_email, password, delay_days=358):
+    try:
+        tnow = dt.datetime.now()
+        send_time = tnow + dt.timedelta(days=358)
+        reminder_subject = f"Reminder:"
+        reminder_body = f"Heyyo Good day\nYou have yet to refresh your subsctiption.\nIf it has not been done by {delay_days}\nIt shall be taken away fron you:\n\n"   
+        delay_seconds = (send_time - tnow).total_seconds() 
+        if delay_seconds > 0:
+            time.sleep(delay_seconds)
+        send_email(from_email, to_email, password, reminder_subject, reminder_body)
+    except Exception as e:
+        print(f"Error error wee woo wee woo the 7-day delay aint working:{e}")  
     
 if __name__ == '__main__':
     from_email = "d9006714@gmail.com"
@@ -51,3 +73,4 @@ if __name__ == '__main__':
     
     Testing testing 1,2,3"""
     delay_email(from_email, to_email, password, subject, body)   
+    send_reminder(from_email, to_email, password, delay_days=358)
